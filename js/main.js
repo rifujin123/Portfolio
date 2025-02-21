be0fec89e29ce376a3e5a1a69079f553ec5eb516
IamRifujin <tuankhoi000@gmail.com>
2025-02-21 17:18:31 +0700

    Success

--- js/main.js
+++ js/main.js
@@ -1,8 +1,16 @@
 //menu
 const menu=document.querySelector(".navbar_links")
 const menuButton=document.querySelector(".navbar_icons")
+const overlat=document.querySelector("#overlay")
 
 menuButton.addEventListener("click",()=>{
     menu.classList.toggle("navbar_open");
     menuButton.classList.toggle('open');
+    overlay.classList.toggle("show");
 });
+
+overlay.addEventListener("click",()=>{
+    menu.classList.toggle("navbar_open");
+    menuButton.classList.toggle("open");
+    overlay.classList.toggle("show");
+})
